4e2e6e008bcc66c7ed755ea25f49ca8391e1f6ae
ibusquim <igorbusquim2019@gmail.com>
2025-07-02 08:51:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+alert('Bem-vindo ao Jogo');
 
+let quantidade = parseInt(prompt('Quantos números você deseja usar para calcular a média?'));
+let soma = 0;
+let contador = 1;
+
+while (contador <= quantidade) {
+  let numero = parseFloat(prompt(`Digite o ${contador}º número:`));
+  soma = soma + numero;
+  contador++;
+}
+
+let media = soma / quantidade;
+
+alert(`A média dos ${quantidade} números digitados é: ${media.toFixed(2)}.`);
